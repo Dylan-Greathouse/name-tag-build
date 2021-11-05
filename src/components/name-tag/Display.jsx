b4ae49df5cf56ac8e8164cf3577391cb5677f215
Dylan-Greathouse <Greathouse.dylanj@gmail.com>
2021-11-05 15:52:32 -0700

    pushing changes up for build

--- src/components/name-tag/Display.jsx
+++ src/components/name-tag/Display.jsx
@@ -1,8 +1,23 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Display = () => ();
+export default function Display({ name }) {
+    return (
+    <>
+      <div class="top" id="top-div">
+        <h1>Hello</h1>
+        <h2>my name is</h2>
+      </div>
+      <div class="middle" id="middle-div">
+        <h3 id="name-output">{name}</h3>
+      </div>
+      <div class="bottom" id="bottom-div">
+
+      </div>
+    </>
+    )
+}
 
 Display.propTypes = {};
 
-export default Display;
+
--- src/containers/NameTag.jsx
+++ src/containers/NameTag.jsx
@@ -1,5 +1,31 @@
 import React, { Component } from 'react';
+import Display from '../components/name-tag/Display';
+import Controls from '../components/name-tag/Controls';
 
 export default class NameTag extends Component {
+  state = {
+    name: '',
+  };
 
+ 
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.setState({ name: e.target.value });
+        
+  };
+
+  render() {
+    const { name } = this.state;
+
+    return (
+      <>
+        <Controls
+          nameInput={name}
+          nameChange={this.handleSubmit}
+          onSubmit={this.handleSubmit}
+        />
+        <Display name={this.handleSubmit} />
+      </>
+    );
+  }
 }
